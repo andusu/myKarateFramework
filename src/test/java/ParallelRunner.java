beef061040b9beb6715c1662e8d2ab384bf83db3
Andy D <36055273+andussu@users.noreply.github.com>
2021-03-26 23:53:17 +0200

    parallel-runner-san

---
+++ src/test/java/ParallelRunner.java
@@ -0,0 +1,34 @@
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParallelRunner {
+
+    @Test
+    public void testParallel() {
+        //Results results = Runner.parallel(getClass(), 5);
+        Results results = Runner.path("classpath:features").tags("~@ignore").parallel(10);
+        generateReport(results.getReportDir());
+        assertEquals(results.getErrorMessages(), 0, results.getFailCount());
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+
+}
